def26de4411cdd18ef7fe1a4ee8f44d2d68b5759
Maksim Kiselev <hagit4@yandex.ru>
2021-12-26 12:14:38 +0300

    server signal handling

--- internal/server/api/api.go
+++ internal/server/api/api.go
@@ -1,5 +1,12 @@
 package api
 
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
 const (
 	metricTypeGauge   = "gauge"
 	metricTypeCounter = "counter"
@@ -21,5 +28,13 @@ func NewMetricServer(addr string) *metricServer {
 }
 
 func (s *metricServer) ListenAndServe() {
-	s.handler.mux.Run(s.addr)
+	go func() {
+		if err := s.handler.mux.Run(s.addr); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+	<-quit
+	log.Println("Server shutdown...")
 }
