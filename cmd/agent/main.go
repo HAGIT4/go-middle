4bf71ee262f65266d618d202879ed9f2b4bfd0dc
Maksim Kiselev <hagit4@yandex.ru>
2022-01-16 16:27:08 +0300

    config in separate files

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"time"
 
 	"github.com/HAGIT4/go-middle/internal/agent"
-	"github.com/caarlos0/env"
 )
 
 type Config struct {
@@ -15,47 +13,12 @@ type Config struct {
 	PollInterval   time.Duration `env:"POLL_INTERVAL"`
 }
 
-var (
-	addressFlag        *string
-	reportIntervalFlag *time.Duration
-	pollIntervalFlag   *time.Duration
-
-	address        string
-	reportInterval time.Duration
-	pollInterval   time.Duration
-)
-
-func init() {
-	addressFlag = flag.String("a", "localhost:8080", "Server address:port")
-	reportIntervalFlag = flag.Duration("r", 10*time.Second, "Metric report interval")
-	pollIntervalFlag = flag.Duration("p", 2*time.Second, "Metric poll interval")
-}
-
 func main() {
-	flag.Parse()
-	cfg := &Config{}
-	if err := env.Parse(cfg); err != nil {
+	cfg, err := agent.InitConfig()
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	if len(cfg.ServerAddr) == 0 {
-		address = *addressFlag
-	} else {
-		address = cfg.ServerAddr
-	}
-
-	if cfg.ReportInterval == 0 {
-		reportInterval = *reportIntervalFlag
-	} else {
-		reportInterval = cfg.ReportInterval
-	}
-
-	if cfg.PollInterval == 0 {
-		pollInterval = *pollIntervalFlag
-	} else {
-		pollInterval = cfg.PollInterval
-	}
-
-	a := agent.NewAgent(address, pollInterval, reportInterval)
+	a := agent.NewAgent(cfg.ServerAddr, cfg.PollInterval, cfg.ReportInterval)
 	a.SendMetricsWithInterval()
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,75 +1,17 @@
 package main
 
 import (
-	"flag"
 	"log"
-	"time"
 
 	"github.com/HAGIT4/go-middle/internal/server/api"
-	"github.com/HAGIT4/go-middle/pkg/models"
-	"github.com/caarlos0/env"
 )
-
-type Config struct {
-	ServerAddr    string        `env:"ADDRESS"`
-	StoreInterval time.Duration `env:"STORE_INTERVAL"`
-	StoreFile     string        `env:"STORE_FILE"`
-	Restore       bool          `env:"RESTORE"`
-}
-
-var (
-	addressFlag       *string
-	restoreFlag       *bool
-	storeIntervalFlag *time.Duration
-	storeFileFlag     *string
-
-	address       string
-	restore       bool
-	storeInterval time.Duration
-	storeFile     string
-)
-
-func init() {
-	addressFlag = flag.String("a", "localhost:8080", "Server address:port")
-	restoreFlag = flag.Bool("r", true, "True to restore data from file")
-	storeIntervalFlag = flag.Duration("i", 300*time.Second, "Backup to file interval")
-	storeFileFlag = flag.String("f", "/tmp/devops-metrics-db.json", "File to backup")
-}
 
 func main() {
-	flag.Parse()
-	cfg := &Config{}
-	if err := env.Parse(cfg); err != nil {
+	cfg, err := api.InitConfig()
+	if err != nil {
 		log.Fatal(err)
 	}
-
-	if len(cfg.ServerAddr) == 0 {
-		address = *addressFlag
-	} else {
-		address = cfg.ServerAddr
-	}
-
-	restore = cfg.Restore || *restoreFlag
-
-	if cfg.StoreInterval == 0 {
-		storeInterval = *storeIntervalFlag
-	} else {
-		storeInterval = cfg.StoreInterval
-	}
-
-	if len(cfg.StoreFile) == 0 {
-		storeFile = *storeFileFlag
-	} else {
-		storeFile = cfg.StoreFile
-	}
-
-	restoreConfig := &models.RestoreConfig{
-		StoreInterval: storeInterval,
-		StoreFile:     storeFile,
-		Restore:       restore,
-	}
-
-	s, err := api.NewMetricServer(address, restoreConfig)
+	s, err := api.NewMetricServer(cfg.ServerAddr, cfg.RestoreConfig)
 	if err != nil {
 		log.Fatal(err)
 	}
