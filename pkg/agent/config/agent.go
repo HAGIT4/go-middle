e59486fd09c00a49e4d78e0bf5ca492660b793aa
Maksim Kiselev <hagit4@yandex.ru>
2022-02-06 12:35:52 +0300

    fix agent config

--- pkg/agent/config/agent.go
+++ pkg/agent/config/agent.go
@@ -4,8 +4,8 @@ import "time"
 
 type AgentConfig struct {
 	ServerAddr     string        `env:"ADDRESS"`
-	PollInterval   time.Duration `env:"REPORT_INTERVAL"`
-	ReportInterval time.Duration `env:"POLL_INTERVAL"`
+	PollInterval   time.Duration `env:"POLL_INTERVAL"`
+	ReportInterval time.Duration `env:"REPORT_INTERVAL"`
 	HashKey        string        `env:"AGENT_HASH"`
 	Batch          bool          `env:"AGENT_BATCH"`
 }
